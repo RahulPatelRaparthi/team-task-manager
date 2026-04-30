1891d17152a8e3138c3fd933f37680cde0603003
Rahul <raparthirahul555@gmail.com>
2026-05-01 01:34:26 +0530

    fixed port issue

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,7 @@
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-
 def home():
     return {"message": "API running"}
-
-import os
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
